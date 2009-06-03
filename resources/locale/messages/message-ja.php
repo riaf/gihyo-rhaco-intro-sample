97ab7c3684a06cd533e212edd5f675715fce0492
Keisuke SATO <riafweb@gmail.com>
2009-06-03 10:06:33 +0900

    i18n

---
+++ resources/locale/messages/message-ja.php
@@ -0,0 +1,21 @@
+<?php
+Message::add(array(
+"name"=>"名前",
+"created"=>"作成日時",
+"category"=>"カテゴリ",
+"subject"=>"タイトル",
+"description"=>"概要",
+"close"=>"完了",
+"priority"=>"優先度",
+"blocker"=>"最高",
+"critical"=>"高い",
+"major"=>"普通",
+"minor"=>"低い",
+"trivial"=>"最低",
+"updated"=>"更新日時",
+"Add new todo"=>"新しい TODO を作成する",
+"action"=>"操作",
+"todo manager"=>"TODO 管理",
+"list"=>"リスト",
+));
+?>
